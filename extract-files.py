d25478a792fa78a92b2a965e3072c8fc5050e915
Han Sol Jin <hansol@hansol.ca>
2025-04-18 21:33:33 -0700

    l01k: add 1.0 INfc to LGE NFC service definition
    Change-Id: I9062b8305542aa4e8f7a10ea58242d8d49ff53cd

--- extract-files.py
+++ extract-files.py
@@ -22,6 +22,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/vendor.lge.hardware.nfc@1.1.so'
     ): blob_fixup()
         .binary_regex_replace(b'libhidltransport.so', b'libhidlbase_shim.so'),
+    'vendor/etc/init/vendor.lge.hardware.nfc@1.1-service.rc': blob_fixup()
+        .regex_replace(
+            "    interface android.hardware.nfc@1.1::INfc default",
+            "    interface android.hardware.nfc@1.0::INfc default\n    interface android.hardware.nfc@1.1::INfc default"
+        ),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
